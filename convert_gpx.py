9e491ac0f25a5e37dd1532a479f729c1a14177ff
Alex Shires <alexshires@users.noreply.github.com>
2020-06-21 18:18:26 +0100

    update

--- convert_gpx.py
+++ convert_gpx.py
@@ -2,6 +2,7 @@
 Python file to convert GPX and plot distributions
 """
 import os
+import pathlib
 import logging
 import argparse
 from gpx_csv_converter import Converter
@@ -9,11 +10,17 @@ from gpx_csv_converter import Converter
 logger = logging.getLogger(__name__)
 logger.setLevel(10)
 
-INPUT_FILE = "input1.gpx"
-OUTPUT_FILE = "output1.csv"
+INPUT_FILE = "data/strava/input1.gpx"
+OUTPUT_FILE = ".output1.csv"
 
 if __name__ == '__main__':
     logger.debug("running gpx analysis")
     parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", type=str, default=None)
+    parser.add_argument("-o", "--output", type=str, default=None)
     options = parser.parse_args()
-    Converter(input_file=INPUT_FILE, output_file=OUTPUT_FILE)
+    data_dir = pathlib.Path.home() / "Documents" / "data" / "strava"
+    input_file_path = data_dir / options.input
+    output_file_path = data_dir / options.output
+    logger.debug("converting %s to %s", input_file_path, output_file_path)
+    Converter(input_file=input_file_path, output_file=output_file_path)
